3a09d076943465bf1314e779cd52a08545daddc3
Jabes Gileade Duarte do Bonfim <jabes.bonfim@invillia.com>
2024-12-09 16:12:20 -0300

    Atualizando endpoints para usar o banco de dados real

--- tests/test_db.py
+++ tests/test_db.py
@@ -1,5 +1,4 @@
 from dataclasses import asdict
-from datetime import datetime
 
 from sqlalchemy import select
 
@@ -9,10 +8,7 @@ from fast_zero.models import User
 def test_create_user(session, mock_db_time):
     with mock_db_time(model=User) as time:
         new_user = User(
-            username='alice',
-            password='secret',
-            email='teste@test',
-            updated_at=datetime(2024, 1, 1),
+            username='alice', password='secret', email='teste@test'
         )
         session.add(new_user)
         session.commit()
